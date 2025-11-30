41336514b9958acb80e4052f430b73870e5f51ef
LeticiaC <leticiafccampos@gmail.com>
2025-11-30 14:29:56 -0300

    Deletar Tema - estilização e lógica

--- src/components/tema/cardtema/CardTema.tsx
+++ src/components/tema/cardtema/CardTema.tsx
@@ -16,7 +16,7 @@ function CardTema({ tema }: CardTemaProps) {
                     className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2">
                     <button>Editar</button></Link>
 
-                <Link to=''
+                <Link to={`/deletartema/${tema.id}`} 
                     className="text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center">
                     <button>Deletar</button></Link>
             </div>
