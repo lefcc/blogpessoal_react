028b96605d7da8e35afb7805febd6f7504332ea5
LeticiaC <leticiafccampos@gmail.com>
2025-11-30 21:27:36 -0300

    Página perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem"
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem"
 import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens"
 import DeletarTema from "./components/tema/deletartema/DeletarTema"
 import FormTema from "./components/tema/formtema/FormTema"
@@ -9,8 +11,7 @@ import { AuthProvider } from "./contexts/AuthContext"
 import Cadastro from "./pages/cadastro/Cadastro"
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
-import FormPostagem from "./components/postagem/formpostagem/FormPostagem"
-import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem"
+import Perfil from "./pages/perfil/Perfil"
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />7
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
